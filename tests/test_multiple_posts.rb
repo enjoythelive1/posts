08cf8c0aacb90f3fe4ccfe42fc91ec7fb4526ab0
Arturo <enjoythelive1@gmail.com>
2014-11-13 19:03:48 -0400

    Tests mejorados y realizados con exito

--- tests/test_multiple_posts.rb
+++ tests/test_multiple_posts.rb
@@ -1,45 +1,50 @@
 require_relative '../logic/Posts'
 require 'test/unit'
 
-postsfile = File.join(__dir__, "posts.json")
-
 class TestPosts < Test::Unit::TestCase
-    def correctInitialization
-        @postsAccess = Posts.new(postfile)
+    def getPostsInstance
+        @postsfile = File.join(File.dirname(__FILE__), 'posts.json') #__dir__ no lo reconoce en mi version
+        @postsAccess = Posts.new(@postsfile)
+        return @postsAccess
+    end
+
+    def test_1_initialization
+        @postsfile = File.join(File.dirname(__FILE__), 'posts.json') #__dir__ no lo reconoce en mi version
+        @postsAccess = Posts.new(@postsfile)
         assert_not_nil(@postsAccess, "Instancia creada correctamente")
     end
 
-    def findByIDTest
-        post1 = @postsaAccess.findById(1)
+    def test_2_findByID
+        post1 = getPostsInstance.findById(1)
         assert_not_nil(post1, "Se encontro el elemento")
         assert_instance_of(Post, post1, "Es un post")
         assert_equal(1, post1.id, "El id del item encontrado debe de ser 1")
     end
 
-    def findByAuthorTest
-        posts = @postsaAccess.findByAuthor("Pixelated")
-        assert_not_nil(posts, "Se encontro los elementos")
-        assert_instance_of(Array, posts, "Es un array")
-        assert(!posts.empty?, "No debe de estar vacio")
-        assert_equal(3, posts.count, "Deben de haber 3 posts")
-        posts.each do |post|
-            assert_equal("Pixelated", post.author, "Debe de tener como autor a \"Pixelated\"")
-        end
-    end
-
-    def findByTagTest
-        posts = @postsaAccess.findPostsByTag("fitness")
+    def test_3_findByAuthor
+        posts = getPostsInstance.findByAuthor("Pixelated")
         assert_not_nil(posts, "Se encontro los elementos")
         assert_instance_of(Array, posts, "Es un array")
         assert(!posts.empty?, "No debe de estar vacio")
         assert_equal(2, posts.count, "Deben de haber 2 posts")
         posts.each do |post|
-            assert(post.tags.include? "fitness", "Debe de tener el tag \"fitness\"")
+            assert_equal("Pixelated", post.author, "Debe de tener como autor a \"Pixelated\"")
         end
     end
 
-    def countAllTagsTest
-        tags = @postsaAccess.countAllTags
+    def test_4_findByTag
+        posts = getPostsInstance.findByTag("fitness")
+        assert_not_nil(posts, "Se encontro los elementos")
+        assert_instance_of(Array, posts, "Es un array")
+        assert(!posts.empty?, "No debe de estar vacio")
+        assert_equal(2, posts.count, "Deben de haber 2 posts")
+        posts.each do |post|
+            assert(post.tags.include?("fitness"), "Debe de tener el tag \"fitness\"")
+        end
+    end
+
+    def test_5_countAllTags
+        tags = getPostsInstance.countAllTags
         assert_not_nil(tags, "Devolvio un elemento")
         assert_instance_of(Hash, tags, "Devolvio un hash")
         assert(!tags.empty?, "No debe de estar vacio")
--- tests/test_single_post.rb
+++ tests/test_single_post.rb
@@ -1,41 +1,47 @@
 require_relative '../logic/Posts'
 require 'test/unit'
 
-postsfile = File.join(__dir__, "post.json")
 
 class TestPosts < Test::Unit::TestCase
-    def correctInitialization
-        @postsAccess = Posts.new(postfile)
+    def getPostsInstance
+        postsfile = File.join(File.dirname(__FILE__), 'post.json') #__dir__ no lo reconoce en mi version
+        @postsAccess = Posts.new(postsfile)
+        return @postsAccess
+    end
+
+    def test_1_initialization
+        postsfile = File.join(File.dirname(__FILE__), 'post.json') #__dir__ no lo reconoce en mi version
+        @postsAccess = Posts.new(postsfile)
         assert_not_nil(@postsAccess, "Instancia creada correctamente")
     end
 
-    def findByIDTest
-        post1 = @postsaAccess.findById(1)
+    def test_2_findByID
+        post1 = getPostsInstance.findById(1)
         assert_not_nil(post1, "Se encontro el elemento")
         assert_instance_of(Post, post1, "Es un post")
         assert_equal(1, post1.id, "El id del item encontrado debe de ser 1")
     end
 
-    def findByAuthorTest
-        posts = @postsaAccess.findByAuthor("Pixelated")
+    def test_3_findByAuthor
+        posts = getPostsInstance.findByAuthor("Pixelator")
         assert_not_nil(posts, "Se encontro los elementos")
         assert_instance_of(Array, posts, "Es un array")
         assert(!posts.empty?, "No debe de estar vacio")
         assert_equal(1, posts.count, "Deben de haber 1 posts")
         posts.each do |post|
-            assert_equal("Pixelated", post.author, "Debe de tener como autor a \"Pixelated\"")
+            assert_equal("Pixelator", post.author, "Debe de tener como autor a \"Pixelator\"")
         end
     end
 
-    def findByTagTest
-        posts = @postsaAccess.findPostsByTag("fitness")
+    def test_4_findByTag
+        posts = getPostsInstance.findByTag("fitness")
         assert_not_nil(posts, "Se encontro los elementos")
         assert_instance_of(Array, posts, "Es un array")
         assert(posts.empty?, "Debe de estar vacio")
     end
 
-    def countAllTagsTest
-        tags = @postsaAccess.countAllTags
+    def test_5_countAllTags
+        tags = getPostsInstance.countAllTags
         assert_not_nil(tags, "Devolvio un elemento")
         assert_instance_of(Hash, tags, "Devolvio un hash")
         assert(!tags.empty?, "No debe de estar vacio")
